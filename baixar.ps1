161db62cf4dec22d6a9ed11c2d1916f185c669ab
webtvbrasil <webtvbrasil.br@gmail.com>
2025-05-12 04:11:16 -0300

    Update baixar.ps1

--- baixar.ps1
+++ baixar.ps1
@@ -4,3 +4,7 @@ New-Item -Path "C:\updates" -ItemType Directory -Force
 # mostrar user do windows
 $usuarioAtual = $env:USERNAME
 Write-Host "Usuário atual: $usuarioAtual"
+
+if (Test-Path "C:\updates") {
+    Write-Host "Pasta 'updates' criada com sucesso!" -ForegroundColor Green
+}
