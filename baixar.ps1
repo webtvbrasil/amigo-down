0bb1f23f4945d97add70763fab67c6abc0af8cd4
webtvbrasil <webtvbrasil.br@gmail.com>
2025-05-12 03:26:10 -0300

    Update baixar.ps1

--- baixar.ps1
+++ baixar.ps1
@@ -1,2 +1,6 @@
 # create directory for update igo amigo maps
 New-Item -Path "C:\updates" -ItemType Directory -Force
+
+# mostrar user do windows
+$usuarioAtual = $env:USERNAME
+Write-Host "Usuário atual: $usuarioAtual"
